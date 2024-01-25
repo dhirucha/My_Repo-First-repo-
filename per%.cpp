ae9ada88511cc7f30b4a6eaa280b8895ea6250a3
Dheeraj_Chaubey <115943278+dhirucha@users.noreply.github.com>
2024-01-26 00:02:57 +0530

    Add files via upload

---
+++ per%.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+ int main()
+{
+    int marks;
+    int per;
+    double total=800;//Initializing variables
+
+    cout<<"Enter Obtained Marks:-"<<endl;
+    cin>>marks;
+    per=marks/total*100;//Formula
+
+    cout<<"Your Gained per% ="<<per;//Output
+    return 0;
+
+
+};
---
+++ per.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+
+int main()
+{
+    int marks;
+    float per;
+    double total=800;//Initializing variables
+
+       cout<<"Enter obtained Marks:-";
+       cin>>marks;
+       per=Marks/total*100;//Formula
+
+        cout<<"Your Gained per% ="<<per;//Output
+        return 0;
+
+
+}
